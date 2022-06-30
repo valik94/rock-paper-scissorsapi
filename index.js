728f9b8f922d7b565b0a2461933b8bf8dbf7d153
Valentin <valentin.lyash@gmail.com>
2022-06-30 00:26:58 -0400

    added comments to explain the different parts of code and how each function comes together to create the game logic

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const CHOICES = [
   },
   {
     name: "scissors",
-    beats:"paper",
+    beats: "paper",
   },
   {
-    name:"rock",
-    beats:"scissors",
+    name: "rock",
+    beats: "scissors",
   },
 ];
 
@@ -17,28 +17,29 @@ addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
 
-// myapi.com?choice=rock
-async function handleRequest(request) {
-  const {searchParams} = new URL(request.url)
-  const requestChoice = searchParams.get('choice')
-  const userChoice = CHOICES.find((choice)=> choice.name === requestChoice);
 
-  return new Response(JSON.stringify(getResults([userChoice, getRandom()])), {
-    headers: { 'content-type': 'application/json' },
+async function handleRequest(request) {
+  const { searchParams } = new URL(request.url) // myapi.com?choice=rock -- this is searchParams
+  const requestChoice = searchParams.get('choice') //requestChoice from searchParams.get('choice')
+  const userChoice = CHOICES.find((choice)=> choice.name === requestChoice); //find userChoice from user definition.
+
+  return new Response(JSON.stringify(getResults([userChoice, getRandom()])), { //gets back userChoice and randomChoice, feeds to getResults, then strigifies it and sends this as a New Response to the backend of the application
+    headers: { 'content-type': 'application/json' }, //for the headers we return content-type: application-json as an object
   })
 }
 
-function getRandom(){
+function getRandom(){ //random function
   const rand=Math.floor(Math.random() * CHOICES.length) //random number between 0,1, and 2 since only 3 objects in CHOICES array
-  return CHOICES[rand]
+  return CHOICES[rand] //returns randomchoice b/w 0,1 or 2.
 }
 
-function getResults(choices){
-  const userWins = isWinner(choices);
-  const aiWins = isWinner([...choices].reverse());
-  const result = userWins ?"You Win": aiWins ? "You Lose": "Draw";
-
-  const results ={
+function getResults(choices){ //passing in array of 2 choices(usersChoice, aiChoice)
+  const userWins = isWinner(choices); //
+  const aiWins = isWinner([...choices].reverse()); //make a copy of the array and then reverse the order, pass it into isWinner and store in aiWins
+  const result = userWins ?"You Win": aiWins ? "You Lose": "Draw"; //terniary operator for comparison:
+                //if userWins return You Win or if aiWins return YouLose, if none then return Draw
+  
+  const results ={ //returns a results object with user:choice, ai:choice and result:win,lose or draw
     user: choices[0],
     ai: choices[1],
     result: result,
@@ -47,6 +48,6 @@ function getResults(choices){
   return results;
 }
 
-function isWinner(choices){
-  return choices[0].beats === choices[1].name;
+function isWinner(choices){ //function isWinner passing in choices
+  return choices[0].beats === choices[1].name; //see if choices[0] beats choices[1] meaning if UserChoice beats AIChoice
 }
